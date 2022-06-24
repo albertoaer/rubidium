e8947c2d55ebe225b10814fb8e319b43bd0d2fe7
Alberto Elorza <albertoelorza2002@gmail.com>
2022-06-24 11:54:54 +0200

    uri query implementation

--- lib/request.rb
+++ lib/request.rb
@@ -1,10 +1,12 @@
 class Request
-    attr_reader :lines, :method, :version, :attributes, :route, :ext
+    attr_reader :lines, :method, :version, :attributes, :route, :query, :ext
 
     def initialize(input, &block)
         @raw = input
         @lines = input.split("\r\n")
-        @method, @route, @version = @lines[0].split(' ')
+        @method, route, @version = @lines[0].split(' ')
+        @route, query = route.split('?')
+        @query = query&.split(/[\;,&]/)&.map { |v| v.split('=') }&.to_h
         @attributes = @lines[1..-1].map { |field| field.split(': ') if field.length() > 0 }
         @ext = (File.extname @route)[1..-1]
         @ext = @ext.to_sym unless @ext.nil?
