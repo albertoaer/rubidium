508f3688049dae6e08f67f86f544d9e92f26eccd
Alberto Elorza <albertoelorza2002@gmail.com>
2022-06-29 21:00:02 +0200

    html syntax validation

--- lib/renderers/erb_template.rb
+++ lib/renderers/erb_template.rb
@@ -2,9 +2,11 @@ require 'erb'
 require_relative 'raw'
 require_relative '../response'
 
-class ERBRenderer < RawRenderer
+class ERBRenderer < HTMLRenderer
     def render(req)
         template = ERB.new yield(:file, req.path)
-        Response.ok template.result(req.get_binding), 'Content-Type' => 'text/html'
+        res = Response.ok template.result(req.get_binding), 'Content-Type' => 'text/html'
+        validate_html res.body
+        res
     end
 end
--- lib/renderers/html.rb
+++ lib/renderers/html.rb
@@ -1,4 +1,16 @@
+require 'nokogiri'
 require_relative 'raw'
+require_relative '../httperror'
 
 class HTMLRenderer < RawRenderer
+    def render(req)
+        res = super req
+        validate_html res.body
+        res
+    end
+
+    def validate_html(html)
+        doc = Nokogiri::HTML html
+        raise HTTPError.new 500, doc.errors.map { |x| x.to_s }.join(', '), 'HTML Error' unless doc.errors.empty?
+    end
 end
