fc52e96a18286038b0674173bc71069aa8a72f74
Alberto Elorza <albertoelorza2002@gmail.com>
2022-06-30 13:00:44 +0200

    extensible types system

--- lib/renderers/raw.rb
+++ lib/renderers/raw.rb
@@ -1,22 +1,19 @@
 require_relative '../response'
 
 class RawRenderer
+    @@types = {html: "text/html", js: "application/javascript", css: "text/css",
+                txt: "text/plain", json: "application/json", png: "image/png",
+                jpeg: "image/jpeg", jpg: "image/jpg"}
+
+    def append_types(**types)
+        @@types.merge! types
+    end
+
     def render(req)
         Response.new 200, 'Ok', yield(:file, req.path), 'Content-Type' => (content_type req.ext)
     end
 
     def content_type(ext)
-        case ext
-        when :html
-            "text/html"
-        when :js
-            "text/javascript"
-        when :css
-            "text/css"
-        when :txt
-            "text/plain"
-        when :json
-            "application/json"
-        end
+        @@types[ext]
     end
 end
